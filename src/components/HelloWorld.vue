e2e46254fc4ef92c3727891b5908e2f12ced86ac
dengqing <1247748612@qq.com>
2020-09-23 22:34:59 +0800

    改变第一行

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      改变这一条<br>
+      <!-- <p>改变第二行</p>
+      <p>改变第三行</p> -->
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
